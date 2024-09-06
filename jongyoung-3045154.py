50366581f462d977480a50e2c84c7fe5a32a080d
Young Lee <youngleeee@outlook.com>
2024-09-06 12:40:07 +0200

    more text, added adam optimizer

--- jongyoung-3045154.py
+++ jongyoung-3045154.py
@@ -9,3 +9,13 @@ import torch.optim as optim
 
 criterion = nn.BCEWithLogitsLoss()
 
+# Adam-Optimizer initialisieren
+optimizer = optim.Adam(model.parameters(), lr=0.001, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
+
+# def train(model, optimizer, criterion, inputs, labels):
+#     optimizer.zero_grad()
+#     outputs = model(inputs)
+#     loss = criterion(outputs, labels)
+#     loss.backward()
+#     optimizer.step()
+#     return loss.item()
